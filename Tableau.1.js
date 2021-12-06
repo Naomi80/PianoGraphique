46e1f58542b85b3ed160d51ca5b1df4425bac160
Ixales <alexis.ardourel2003@gmail.com>
2021-12-06 14:19:56 +0100

    1

--- Tableau.1.js
+++ Tableau.1.js
@@ -1,11 +1,17 @@
 
 class Tableau1 extends Phaser.Scene {
 
-    preload() {
+    preload(){
         //bg (tout au fond et très flou)
-
+        this.load.image('bg', 'Assets/Background/Fond.png');
         //ground (premier plan noir)
 
 
-        }
     }
+
+    create() {
+        let bg = this.add.image(0, 0, 'bg').setOrigin(0, 0);
+
+
+    }
+}
