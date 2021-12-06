a79ea11752c6c5d76f925297a1371e5d17da00f4
Ixales <alexis.ardourel2003@gmail.com>
2021-12-06 15:54:04 +0100

    1

--- Tableau.1.js
+++ Tableau.1.js
@@ -7,11 +7,6 @@ class Tableau1 extends Phaser.Scene {
         //ground (premier plan noir)
         this.load.image('bubbles', 'Assets/Ground/Bulles/bubbles.png');
         this.load.image('blueBubbles', 'Assets/Ground/Bulles/blue_bubbles.png');
-        this.load.image('greyBubbles', 'Assets/Ground/Bulles/grey_bubbles.png');
-        this.load.image('littleBlueBubbles', 'Assets/Ground/Bulles/ltlle_blue_bubbles.png');
-        this.load.image('littleGreyBubbles', 'Assets/Ground/Bulles/little_grey_bubbles.png');
-        this.load.image('pinkBubbles', 'Assets/Ground/Bulles/pink_bubbles.png');
-        this.load.image('violetBubbles', 'Assets/Ground/Bulles/violet_bubbles.png');
 
 
 
@@ -48,19 +43,6 @@ class Tableau1 extends Phaser.Scene {
             repeat: -1,
             delay: 0,
         });
-        this.littleGreyBubbles = this.add.image(500, 500, 'littleGreyBubbles').setOrigin(0, 0);
-        this.littleGreyBubbles.setVisible(false)
-        this.littleGreyBubbles.setScale(1)
-        this.tweens.add({
-            targets: this.littleGreyBubbles,
-            y: -400,
-            duration: 3000,
-            ease: 'linear',
-            yoyo: false,
-            flipY: false,
-            repeat: -1,
-            delay: 0,
-        });
 
 
 
@@ -88,14 +70,6 @@ class Tableau1 extends Phaser.Scene {
                         me.blueBubbles.setVisible(false)
                     } else {
                         me.blueBubbles.setVisible(true)
-                    }
-                    break
-                // initialisation de la touche en appuie I pour blueBubbles
-                case Phaser.Input.Keyboard.KeyCodes.O:
-                    if (me.littleGreyBubbles.visible == true) {
-                        me.littleGreyBubbles.setVisible(false)
-                    } else {
-                        me.littleGreyBubbles.setVisible(true)
                     }
                     break
 
