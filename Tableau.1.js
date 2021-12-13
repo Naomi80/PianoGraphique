af4671497cce9962106eb7d48fdfd93afd044874
Naomi80 <naomi.morville-nm-80@outlook.com>
2021-12-13 11:55:35 +0100

    1

--- Tableau.1.js
+++ Tableau.1.js
@@ -165,7 +165,7 @@ class Tableau1 extends Phaser.Scene {
         ///petits poissons
         this.fishes1 = this.add.image(-150, 0, 'fishes1').setOrigin(0, 0);
         this.fishes1.setVisible(false)
-        this.fishes1.setScale(0.1)
+        this.fishes1.setScale(0.08)
         this.tweens.add({
             targets: this.fishes1,
             x: 1000,
@@ -178,7 +178,7 @@ class Tableau1 extends Phaser.Scene {
         });
         this.fishes2 = this.add.image(-100, 100, 'fishes2').setOrigin(0, 0);
         this.fishes2.setVisible(false)
-        this.fishes2.setScale(0.08)
+        this.fishes2.setScale(0.07)
         this.tweens.add({
             targets: this.fishes2,
             x: 900,
@@ -191,7 +191,7 @@ class Tableau1 extends Phaser.Scene {
         });
         this.fishes3 = this.add.image(750, 150, 'fishes3').setOrigin(0, 0);
         this.fishes3.setVisible(false)
-        this.fishes3.setScale(0.2)
+        this.fishes3.setScale(0.150)
         this.tweens.add({
             targets: this.fishes3,
             x: -700,
@@ -202,9 +202,9 @@ class Tableau1 extends Phaser.Scene {
             repeat: -1,
             delay: 0,
         });
-        this.fishes4 = this.add.image(850, 175, 'fishes4').setOrigin(0, 0);
+        this.fishes4 = this.add.image(850, 185, 'fishes4').setOrigin(0, 0);
         this.fishes4.setVisible(false)
-        this.fishes4.setScale(0.1)
+        this.fishes4.setScale(0.085)
         this.tweens.add({
             targets: this.fishes4,
             x: -900,
@@ -217,7 +217,7 @@ class Tableau1 extends Phaser.Scene {
         });
         this.fishes5 = this.add.image(-150, 440, 'fishes5').setOrigin(0, 0);
         this.fishes5.setVisible(false)
-        this.fishes5.setScale(0.125)
+        this.fishes5.setScale(0.110)
         this.tweens.add({
             targets: this.fishes5,
             x: 900,
@@ -230,7 +230,7 @@ class Tableau1 extends Phaser.Scene {
         });
         this.fishes6 = this.add.image(850, 200, 'fishes6').setOrigin(0, 0);
         this.fishes6.setVisible(false)
-        this.fishes6.setScale(0.160)
+        this.fishes6.setScale(0.120)
         this.tweens.add({
             targets: this.fishes6,
             x: -900,
@@ -241,9 +241,138 @@ class Tableau1 extends Phaser.Scene {
             repeat: -1,
             delay: 0,
         });
-        this.ryuboshi = this.add.image(300, 0, 'ryuboshi').setOrigin(0, 0);
+        ///Gros poissons
+        this.laboon = this.add.image(850, 50, 'laboon').setOrigin(0, 0);
+        this.laboon.setVisible(false)
+        this.laboon.setScale(0.1)
+        this.laboon.setAlpha(0.95)
+        this.tweens.add({
+            targets: this.laboon,
+            x: -900,
+            duration: 7500,
+            ease: 'linear',
+            yoyo: false,
+            flipY: false,
+            repeat: -1,
+            delay: 0,
+        });
+        this.megalo = this.add.image(-150, 300, 'megalo').setOrigin(0, 0);
+        this.megalo.setVisible(false)
+        this.megalo.setScale(0.1)
+        this.megalo.setAlpha(0.95)
+        this.tweens.add({
+            targets: this.megalo,
+            x: 900,
+            duration: 6500,
+            ease: 'linear',
+            yoyo: false,
+            flipY: false,
+            repeat: -1,
+            delay: 0,
+        });
+        this.meuhmeuh = this.add.image(250, 185, 'meuhmeuh').setOrigin(0, 0);
+        this.meuhmeuh.setVisible(false)
+        this.meuhmeuh.setScale(0.075)
+        this.meuhmeuh.setAlpha(0.9)
+        this.tweens.add({
+            targets: this.meuhmeuh,
+            y: -100,
+            duration: 9000,
+            ease: 'linear',
+            yoyo: true,
+            flipY: false,
+            repeat: -1,
+            delay: 0,
+        });
+        this.seaBoar = this.add.image(570, 300, 'seaBoar').setOrigin(0, 0);
+        this.seaBoar.setVisible(false)
+        this.seaBoar.setScale(0.1)
+        this.seaBoar.setAlpha(1)
+
+        this.seaKing = this.add.image(820, 270, 'seaKing').setOrigin(0, 0);
+        this.seaKing.setVisible(false)
+        this.seaKing.setScale(0.130)
+        this.seaKing.setAlpha(1)
+
+        ///Hommes poissons
+        this.neptune = this.add.image(200, 525, 'neptune').setOrigin(0, 0);
+        this.neptune.setVisible(false)
+        this.neptune.setScale(0.2)
+        this.tweens.add({
+            targets: this.neptune,
+            y: -310,
+            duration: 8000,
+            ease: 'linear',
+            yoyo: true,
+            flipY: false,
+            repeat: -1,
+            delay: 0,
+        });
+        this.pappug = this.add.image(-20, 445, 'pappug').setOrigin(0, 0);
+        this.pappug.setVisible(false)
+        this.pappug.setScale(0.110)
+        this.pappug.setAlpha(1)
+
+        this.ryuboshi = this.add.image(395, 525, 'ryuboshi').setOrigin(0, 0);
         this.ryuboshi.setVisible(false)
-        this.ryuboshi.setScale(0.3)
+        this.ryuboshi.setScale(0.150)
+        this.tweens.add({
+            targets: this.ryuboshi,
+            y: -350,
+            duration: 7000,
+            ease: 'linear',
+            yoyo: true,
+            flipY: false,
+            repeat: -1,
+            delay: 0,
+        });
+        this.shirahoshi = this.add.image(-150, 400, 'shirahoshi').setOrigin(0, 0);
+        this.shirahoshi.setVisible(false)
+        this.shirahoshi.setScale(0.135)
+        this.shirahoshi.setAlpha(1)
+        this.tweens.add({
+            targets: this.shirahoshi,
+            x: 900,
+            duration: 6500,
+            ease: 'linear',
+            yoyo: false,
+            flipY: false,
+            repeat: -1,
+            delay: 0,
+        });
+        this.octy = this.add.image(900, 50, 'octy').setOrigin(0, 0);
+        this.octy.setVisible(false)
+        this.octy.setScale(0.150)
+        this.octy.setAlpha(2)
+        this.tweens.add({
+            targets: this.octy,
+            x:-700,
+            y: 500,
+            duration: 5000,
+            ease: 'linear',
+            yoyo: false,
+            flipY: true,
+            flipX: true,
+            repeat: -1,
+            delay: 0,
+        });
+        this.siren = this.add.image(850, 50, 'siren').setOrigin(0, 0);
+        this.siren.setVisible(false)
+        this.siren.setScale(0.1)
+        this.siren.setAlpha(2)
+        this.tweens.add({
+            targets: this.siren,
+            x: -900,
+            duration: 7500,
+            ease: 'linear',
+            yoyo: false,
+            flipY: false,
+            repeat: -1,
+            delay: 0,
+        });
+
+
+
 
         this.initKeyboard();
 
@@ -377,6 +506,99 @@ class Tableau1 extends Phaser.Scene {
                         me.fishes6.setVisible(true)
                     }
                     break
+                ///Gros poissons
+                // initialisation de la touche en push H pour laboon
+                case Phaser.Input.Keyboard.KeyCodes.H:
+                    if (me.laboon.visible == true) {
+                        me.laboon.setVisible(false)
+                    } else {
+                        me.laboon.setVisible(true)
+                    }
+                    break
+                // initialisation de la touche en push J pour megalo
+                case Phaser.Input.Keyboard.KeyCodes.J:
+                    if (me.megalo.visible == true) {
+                        me.megalo.setVisible(false)
+                    } else {
+                        me.megalo.setVisible(true)
+                    }
+                    break
+                // initialisation de la touche en push K pour meuhmeuh
+                case Phaser.Input.Keyboard.KeyCodes.K:
+                    if (me.meuhmeuh.visible == true) {
+                        me.meuhmeuh.setVisible(false)
+                    } else {
+                        me.meuhmeuh.setVisible(true)
+                    }
+                    break
+                // initialisation de la touche en push L pour seaBoar
+                case Phaser.Input.Keyboard.KeyCodes.L:
+                    if (me.seaBoar.visible == true) {
+                        me.seaBoar.setVisible(false)
+                    } else {
+                        me.seaBoar.setVisible(true)
+                    }
+                    break
+                // initialisation de la touche en push M pour seaKing
+                case Phaser.Input.Keyboard.KeyCodes.M:
+                    if (me.seaKing.visible == true) {
+                        me.seaKing.setVisible(false)
+                    } else {
+                        me.seaKing.setVisible(true)
+                    }
+                    break
+                ///Hommes poissons
+                // initialisation de la touche en push W pour neptune
+                case Phaser.Input.Keyboard.KeyCodes.W:
+                    if (me.neptune.visible == true) {
+                        me.neptune.setVisible(false)
+                    } else {
+                        me.neptune.setVisible(true)
+                    }
+                    break
+                // initialisation de la touche en push X pour ryuboshi
+                case Phaser.Input.Keyboard.KeyCodes.X:
+                    if (me.ryuboshi.visible == true) {
+                        me.ryuboshi.setVisible(false)
+                    } else {
+                        me.ryuboshi.setVisible(true)
+                    }
+                    break
+
+                // initialisation de la touche en push C pour pappug
+                case Phaser.Input.Keyboard.KeyCodes.C:
+                    if (me.pappug.visible == true) {
+                        me.pappug.setVisible(false)
+                    } else {
+                        me.pappug.setVisible(true)
+                    }
+                    break
+                // initialisation de la touche en push V pour shirahoshi
+                case Phaser.Input.Keyboard.KeyCodes.V:
+                    if (me.shirahoshi.visible == true) {
+                        me.shirahoshi.setVisible(false)
+                    } else {
+                        me.shirahoshi.setVisible(true)
+                    }
+                    break
+                // initialisation de la touche en push B pour octy
+                case Phaser.Input.Keyboard.KeyCodes.B:
+                    if (me.octy.visible == true) {
+                        me.octy.setVisible(false)
+                    } else {
+                        me.octy.setVisible(true)
+                    }
+                    break
+                // initialisation de la touche en push N pour siren
+                case Phaser.Input.Keyboard.KeyCodes.N:
+                    if (me.siren.visible == true) {
+                        me.siren.setVisible(false)
+                    } else {
+                        me.siren.setVisible(true)
+                    }
+                    break
+
+
 
 
 
